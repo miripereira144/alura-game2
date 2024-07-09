adceaafaf06539f0b6d1d8e681f60ff6ad0661eb
miripereira144 <155463876+miripereira144@users.noreply.github.com>
2024-07-08 23:12:58 -0300

    prueba de evento
    onclick Javascript.

---
+++ app.js
@@ -0,0 +1,13 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del Numero Secreto';
+
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Indica un numero del 1 al 10';
+
+function intentoDeUsuario(){
+    alert('click desde el boton')
+ }
+
+
+ 
